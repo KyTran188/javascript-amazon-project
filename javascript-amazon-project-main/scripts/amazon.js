55f8bbba73ef9c16f841d2d2675940782c70fe49
KyTran188 <lycafesuayeuthuong@gmail.com>
2023-06-18 17:17:00 +0700

    add cart js

--- javascript-amazon-project-main/scripts/amazon.js
+++ javascript-amazon-project-main/scripts/amazon.js
@@ -17,7 +17,7 @@ products.forEach((product) => {
   <div class="product-rating-container">
     <img
       class="product-rating-stars"
-      src="images/ratings/rating-${product.rating.starts * 10}.png"
+      src="images/ratings/rating-${product.rating.stars * 10}.png"
     />
     <div class="product-rating-count link-primary">${product.rating.count}</div>
   </div>
@@ -46,7 +46,29 @@ products.forEach((product) => {
     Added
   </div>
 
-  <button class="add-to-cart-button button-primary">Add to Cart</button>
+  <button class="add-to-cart-button button-primary js-add-to-cart " 
+  data-product-id="${product.id}"
+  >Add to Cart</button>
 </div>`;
 });
 document.querySelector(".js-products").innerHTML = productsHTML;
+document.querySelectorAll(".js-add-to-cart").forEach((button) => {
+  button.addEventListener("click", () => {
+    let productId = button.dataset.productId;
+    let matchingItem;
+    cart.forEach((item) => {
+      if (productId === item.productId) {
+        matchingItem = item;
+      }
+    });
+    if (matchingItem) {
+      matchingItem.quantity++;
+    } else {
+      cart.push({
+        productId: productId,
+        quantity: 1,
+      });
+    }
+    console.log(cart);
+  });
+});
